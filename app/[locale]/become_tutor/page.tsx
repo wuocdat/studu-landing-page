c869cf2c7ec55544134a1b080c699959753799f2
wuocdat <77782849+wuocdat@users.noreply.github.com>
2025-03-16 01:00:17 +0700

    add TutorBenefitSection and StutuFeedback components to BecomeTutorPage; implement benefits display with localization support

--- app/[locale]/become_tutor/page.tsx
+++ app/[locale]/become_tutor/page.tsx
@@ -1,10 +1,14 @@
 import { Stack } from '@mantine/core';
+import StutuFeedback from '@/components/StutuFeedback';
+import TutorBenefitSection from './subComponents/BenefitSection';
 import StepsSection from './subComponents/StepsSection';
 
 const BecomeTutorPage = () => {
   return (
     <Stack py={100}>
       <StepsSection />
+      <TutorBenefitSection />
+      <StutuFeedback />
     </Stack>
   );
 };
